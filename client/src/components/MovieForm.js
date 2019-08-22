94a38dde5f9fd06564af60ae5c16115ff92aade7
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-22 16:00:16 -0700

    git trouble

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
-const Basic = ({ touched, errors, tos }) => {
+const update = movie => {
+  console.log(movie)
+  axios
+    .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
+    .then(console.log)
+    .catch(console.log)
+};
+
+const Basic = ({ touched, errors }) => {
   return (
     <Form>
       <div>
@@ -18,8 +27,8 @@ const Basic = ({ touched, errors, tos }) => {
         {touched.metascore && errors.metascore && <p>{errors.metascore}</p>}
       </div>
       <div>
-        <Field type='text' name='actors' placeholder='actors' />
-        {touched.actors && errors.actors && <p>{errors.actors}</p>}
+        <Field type='text' name='stars' placeholder='stars' />
+        {touched.stars && errors.stars && <p>{errors.stars}</p>}
       </div>
       <button type='submit'>submit</button>
     </Form>
@@ -32,8 +41,9 @@ export default withFormik({
       title: props.title || '',
       director: props.director || '',
       metascore: props.metascore || '',
-      actors: props.actors || '',
-      subFun: props.subFun
+      stars: props.stars || '',
+      id: props.match.params.id,
+      subFun: update
     };
   },
 
@@ -46,15 +56,22 @@ export default withFormik({
       .required("title can't be empty"),
     metascore: Yup
       .number()
-      .min(0, "cannot have negative score")
-      .max(100, "max score is 100")
-      .required("score required"),
+      .min(0, 'cannot have negative score')
+      .max(100, 'max score is 100')
+      .required('score required'),
     actors: Yup
       .string()
   }),
 
   handleSubmit(vals, { resetForm }) {
-    vals.subFun(vals);
+    const movie = {
+      id: vals.id,
+      title: vals.title,
+      director: vals.director,
+      metascore: vals.metascore,
+      stars: vals.stars.split(',')
+    };
+    vals.subFun(movie);
     resetForm();
   }
 })(Basic);
